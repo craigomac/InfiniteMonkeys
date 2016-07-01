3f8d1f13027337ad537360298fc4f296f39c58bb
Craig McMahon <craigthebrit@gmail.com>
2016-07-01 14:19:23 +0200

    Initial Commit

---
+++ InfiniteMonkeys/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  InfiniteMonkeys
+//
+//  Created by Mcmahon, Craig on 7/1/16.
+//  Copyright © 2016 handform. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ InfiniteMonkeys/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  InfiniteMonkeys
+//
+//  Created by Mcmahon, Craig on 7/1/16.
+//  Copyright © 2016 handform. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
