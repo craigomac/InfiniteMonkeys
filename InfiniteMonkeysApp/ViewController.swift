afd0c97fb24acaf30d5759c02fe7928dced00763
Alejandro Isaza <al@isaza.ca>
2016-07-10 21:33:56 -0700

    Add seed text field an diversity to iOS app
    Also don't reload weights on every restart.

--- InfiniteMonkeysApp/ViewController.swift
+++ InfiniteMonkeysApp/ViewController.swift
@@ -10,60 +10,102 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var seedTextField: UITextField!
+    @IBOutlet weak var diversitySlider: UISlider!
     @IBOutlet weak var startStopButton: UIButton!
     @IBOutlet weak var textView: UITextView!
     
     var poet: Poet?
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let path = NSBundle.mainBundle().pathForResource("lstm_text_generation_weights", ofType: "h5") else {
+            fatalError("Weigths file not found")
+        }
+        poet = Poet(pathToTrainedWeights: path)
+
         #if arch(i386) || arch(x86_64)
-            assert(false, "This app will not function on the iOS Simulator because of Metal-dependent functionality.")
+            preconditionFailure("This app will not function on the iOS Simulator because of Metal-dependent functionality.")
         #endif
     }
 
     @IBAction func tappedStartStopButton(sender: AnyObject?) {
-        if let poet = self.poet {
-            poet.stopEvaluating()
-            startStopButton.setTitle("Start", forState: .Normal)
-            self.poet = nil
+        if poet?.isEvaluating ?? false {
+            stop()
+        } else {
+            start()
+        }
+    }
+
+    func start() {
+        guard let poet = poet else {
             return
         }
-        
-        startStopButton.enabled = false
-        startStopButton.setTitle("Stop", forState: .Normal)
-        
-        self.textView.text = "..."
 
-        let seed = "Once upon a time"
+        disableControls()
+        startStopButton.setTitle("Stop", forState: .Normal)
+
+        if !poet.isPrepared {
+            prepare {
+                self.start()
+            }
+            return
+        }
+
         var buffer: String = ""
         var count = 0
-        
-        if let path = NSBundle.mainBundle().pathForResource("lstm_text_generation_weights", ofType: "h5") {
-            poet = Poet(pathToTrainedWeights: path)
-            poet!.prepareToEvaluate { (prepared) in
+
+        textView.text = seedTextField.text
+        poet.temperature = diversitySlider.value
+        poet.startEvaluating(seed: seedTextField.text!) { string in
+            buffer = buffer + string
+            count += 1
+
+            if count > 5 {
+                let bufferCopy = buffer
                 dispatch_async(dispatch_get_main_queue()) {
-                    self.startStopButton.enabled = true
-                    self.textView.text = seed
+                    self.textView.text = self.textView.text + bufferCopy
                 }
 
-                self.poet!.startEvaluating(seed: seed) { (string) in
-                    buffer = buffer + string
-                    count += 1
+                count = 0
+                buffer = ""
+            }
+        }
+    }
 
-                    if count > 5 {
-                        let bufferCopy = buffer
-                        dispatch_async(dispatch_get_main_queue()) {
-                            self.textView.text = self.textView.text + bufferCopy
-                        }
-
-                        count = 0
-                        buffer = ""
-                    }
+    func prepare(completion: () -> Void) {
+        textView.text = "Loading..."
+        startStopButton.enabled = false
+        poet?.prepareToEvaluate { prepared in
+            dispatch_async(dispatch_get_main_queue()) {
+                self.startStopButton.enabled = true
+                if prepared {
+                    completion()
+                } else {
+                    self.textView.text = nil
                 }
             }
         }
     }
 
+    func stop() {
+        poet?.stopEvaluating()
+        enableControls()
+        startStopButton.setTitle("Start", forState: .Normal)
+    }
+
+    func disableControls() {
+        seedTextField.resignFirstResponder()
+        seedTextField.enabled = false
+        diversitySlider.enabled = false
+    }
+
+    func enableControls() {
+        seedTextField.enabled = true
+        diversitySlider.enabled = true
+        startStopButton.enabled = true
+    }
 
 }
 
